737aba2d24a747d2cb2bb913eedc711066e7810f
Dulsara Dilitha <41503317+Dula95@users.noreply.github.com>
2020-05-06 18:13:45 +0530

    Add database

---
+++ paf_testing.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: paf
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `testing`
+--
+
+DROP TABLE IF EXISTS `testing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `testing` (
+  `testId` int NOT NULL AUTO_INCREMENT,
+  `testName` varchar(25) NOT NULL,
+  `tDescription` varchar(50) NOT NULL,
+  `tDate` varchar(15) NOT NULL,
+  `tTime` varchar(15) NOT NULL,
+  PRIMARY KEY (`testId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `testing`
+--
+
+LOCK TABLES `testing` WRITE;
+/*!40000 ALTER TABLE `testing` DISABLE KEYS */;
+INSERT INTO `testing` VALUES (1,'MRI','MRI Scan - Saman Kumara','09/06/2020','9.30 A.M.'),(2,'CT','CT scan - Nihal Perera','11/05/2020','7.00 A.M.'),(3,'Blood Testing','Blood Testing - Dulini Perera','20/07/2020','4.30 P.M'),(4,'Urine Testing','Urine testing - Mr. Wikramasighe','03/06/2020','7.30 P.M.');
+/*!40000 ALTER TABLE `testing` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-06 18:09:02
